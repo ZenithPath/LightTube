3a7d8be410dd85c993c15bbc4a838df8ce3232a7
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-07-26 00:20:16 +0300

    initial commit; added libraries

---
+++ app/src/main/java/com/example/scame/lighttube/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.scame.lighttube;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        ButterKnife.bind(this);
+    }
+}
