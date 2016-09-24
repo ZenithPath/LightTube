95fa2aa636fc39c89281e281ba9d1b1c238dddb1
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-09-24 20:25:27 +0300

    added custom full screen handling (youtube player)

--- app/src/main/java/com/example/scame/lighttube/presentation/activities/PlayVideoActivity.java
+++ app/src/main/java/com/example/scame/lighttube/presentation/activities/PlayVideoActivity.java
@@ -1,35 +1,149 @@
 package com.example.scame.lighttube.presentation.activities;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
+import com.example.scame.lighttube.PrivateValues;
 import com.example.scame.lighttube.R;
-import com.example.scame.lighttube.presentation.di.components.ApplicationComponent;
-import com.example.scame.lighttube.presentation.fragments.PlayVideoFragment;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class PlayVideoActivity extends BaseActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    public static final String PLAY_VIDEO_FRAG_TAG = "playVideo";
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+public class PlayVideoActivity extends YouTubeFailureRecoverActivity implements CompoundButton.OnCheckedChangeListener,
+                                                                                YouTubePlayer.OnFullscreenListener {
+
+    private static final int PORTRAIT_ORIENTATION = Build.VERSION.SDK_INT < 9
+            ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            : ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT;
+
+    @BindView(R.id.player)
+    YouTubePlayerView playerView;
+
+    @BindView(R.id.fullscreen_button)
+    Button fullscreenButton;
+
+    @BindView(R.id.landscape_fullscreen_checkbox)
+    CompoundButton fullscreenCheckBox;
+
+    @BindView(R.id.other_views)
+    View otherViews;
+
+    @BindView(R.id.play_base_layout)
+    LinearLayout baseLayout;
+
+    private YouTubePlayer youTubePlayer;
+
+    private boolean fullscreen;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_video_activity);
 
-        setupFragment();
+        ButterKnife.bind(this);
+
+        fullscreenCheckBox.setOnCheckedChangeListener(this);
+        fullscreenButton.setOnClickListener(view -> youTubePlayer.setFullscreen(!fullscreen));
+
+        playerView.initialize(PrivateValues.API_KEY, this);
+
+        doLayout();
     }
 
     @Override
-    protected void inject(ApplicationComponent appComponent) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        int controlFlags = youTubePlayer.getFullscreenControlFlags();
 
+        if (isChecked) {
+            setRequestedOrientation(PORTRAIT_ORIENTATION);
+            controlFlags |= YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE;
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+            controlFlags &= ~YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE;
+        }
+
+        youTubePlayer.setFullscreenControlFlags(controlFlags);
     }
 
-    void setupFragment() {
-        Bundle args = new Bundle();
-        args.putString(getString(R.string.video_id), getIntent().getStringExtra(getString(R.string.video_id)));
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return playerView;
+    }
 
-        PlayVideoFragment fragment = new PlayVideoFragment();
-        fragment.setArguments(args);
-        replaceFragment(R.id.play_video_fl, fragment, PLAY_VIDEO_FRAG_TAG);
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        this.youTubePlayer = youTubePlayer;
+
+        setControlsEnabled();
+        // Specify that we want to handle fullscreen behavior ourselves.
+        this.youTubePlayer.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
+        this.youTubePlayer.setOnFullscreenListener(this);
+
+        if (!wasRestored) {
+            this.youTubePlayer.cueVideo(getIntent().getStringExtra(getString(R.string.video_id)));
+        }
+    }
+
+    private void doLayout() {
+        LinearLayout.LayoutParams playerParams = (LinearLayout.LayoutParams) playerView.getLayoutParams();
+        if (fullscreen) {
+            // When in fullscreen, the visibility of all other views than the player should be set to
+            // GONE and the player should be laid out across the whole screen.
+            playerParams.width = MATCH_PARENT;
+            playerParams.height = MATCH_PARENT;
+
+            otherViews.setVisibility(View.GONE);
+        } else {
+            otherViews.setVisibility(View.VISIBLE);
+            ViewGroup.LayoutParams otherViewsParams = otherViews.getLayoutParams();
+
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                playerParams.width = otherViewsParams.width = 0;
+                playerParams.height = WRAP_CONTENT;
+                otherViewsParams.height = MATCH_PARENT;
+                playerParams.weight = 1;
+
+                baseLayout.setOrientation(LinearLayout.HORIZONTAL);
+            } else {
+                playerParams.width = otherViewsParams.width = MATCH_PARENT;
+                playerParams.height = WRAP_CONTENT;
+                playerParams.weight = 0;
+                otherViewsParams.height = 0;
+
+                baseLayout.setOrientation(LinearLayout.VERTICAL);
+            }
+            setControlsEnabled();
+        }
+    }
+
+    private void setControlsEnabled() {
+        fullscreenCheckBox.setEnabled(youTubePlayer != null
+                && getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
+        fullscreenButton.setEnabled(youTubePlayer != null);
+    }
+
+    @Override
+    public void onFullscreen(boolean isFullscreen) {
+        fullscreen = isFullscreen;
+        doLayout();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        doLayout();
     }
 }
---
+++ app/src/main/java/com/example/scame/lighttube/presentation/activities/YouTubeFailureRecoverActivity.java
@@ -0,0 +1,34 @@
+package com.example.scame.lighttube.presentation.activities;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.example.scame.lighttube.PrivateValues;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+public abstract class YouTubeFailureRecoverActivity extends YouTubeBaseActivity
+                                    implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            Toast.makeText(this, errorReason.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(PrivateValues.API_KEY, this);
+        }
+    }
+
+    protected abstract YouTubePlayer.Provider getYouTubePlayerProvider();
+}
